8838da7f7c092681b6dac0136edee7a9f91d75a3
xcaliber-tech <73225624+xcaliber-tech@users.noreply.github.com>
2022-12-12 09:20:03 +0100

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,10 @@
-let timer = 0
 input.onButtonPressed(Button.A, function () {
-    timer = randint(5, 15)
-    basic.showIcon(IconNames.Chessboard)
-    while (timer > 0) {
-        timer += -1
-        basic.pause(1000)
-    }
-    basic.showIcon(IconNames.Skull)
+    radio.sendValue(imie, timer)
+    basic.showIcon(IconNames.Yes)
 })
-basic.forever(function () {
-	
-})
+let timer = 0
+let imie = ""
+basic.showIcon(IconNames.Chessboard)
+imie = "bartek"
+timer = randint(30, 60)
+radio.setGroup(74)
